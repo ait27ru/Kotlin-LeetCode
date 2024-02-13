eef2e7e5d24b1ddf58702e8a18ff4525f3333376
ait27ru <ait27ru@gmail.com>
2024-02-13 20:29:38 +1300

    Add a solution to the PerfectSquares problem. Add unit tests.

---
+++ src/main/kotlin/PerfectSquares.kt
@@ -0,0 +1,32 @@
+import kotlin.math.min
+
+/*
+    https://leetcode.com/problems/perfect-squares/description/
+    Given an integer n, return the least number of perfect square numbers that sum to n.
+    A perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
+    For example, 1, 4, 9, and 16 are perfect squares while 3 and 11 are not.
+*/
+
+class PerfectSquares {
+
+    fun numSquares(n: Int): Int {
+        // task constraints
+        require(n in 1..10_000)
+
+        val dp = IntArray(n + 1)
+        dp.fill(n)
+        dp[0] = 0
+        dp[1] = 1
+        for (targetSquare in 1..n) {
+            for (side in 1..targetSquare) {
+                val currentSquare = side * side
+                val restSquare = targetSquare - currentSquare
+                if (restSquare < 0) {
+                    break
+                }
+                dp[targetSquare] = min(dp[targetSquare], 1 + dp[restSquare])
+            }
+        }
+        return dp[n]
+    }
+}
---
+++ src/test/kotlin/PerfectSquaresTest.kt
@@ -0,0 +1,49 @@
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class PerfectSquaresTest {
+
+    private var solution = PerfectSquares()
+
+    @BeforeEach
+    fun setUp() {
+        solution = PerfectSquares()
+    }
+
+    @Test
+    fun numSquares_ShouldSatisfy_MinValueEdgeCase() {
+        val num = solution.numSquares(1)
+        // 1 = 1 (1x1)
+        assertEquals(1, num)
+    }
+
+    @Test
+    fun numSquares_ShouldSatisfy_MaxValueEdgeCase() {
+        val num = solution.numSquares(10_000)
+        // 10000 = 10000 (100x100)
+        assertEquals(1, num)
+    }
+
+    @Test
+    fun numSquares_ShouldSatisfy_Example1() {
+        val num = solution.numSquares(12)
+        // 12 = 4 + 4 + 4
+        assertEquals(3, num)
+    }
+
+    @Test
+    fun numSquares_ShouldSatisfy_Example2() {
+        val num = solution.numSquares(13)
+        // 13 = 4 + 9
+        assertEquals(2, num)
+    }
+
+    @Test
+    fun numSquares_ShouldSatisfy_Example3() {
+        val num = solution.numSquares(5)
+        // 5 = 4 + 1
+        assertEquals(2, num)
+    }
+}
